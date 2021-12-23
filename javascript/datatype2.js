e92a8afefc447a6d1bfb60ff1969d85ecc2668b4
aakashdeveloper <ahanda205@gmail.com>
2021-12-23 09:52:15 +0530

    datatypes

---
+++ javascript/datatype2.js
@@ -0,0 +1,17 @@
+var a = true
+var b = true
+a+b
+true+true
+2
+true+false
+1
+10+true
+11
+"hi"+true
+'hitrue'
+10/false
+Infinity
+
+true is equal to 1
+false is equal to 0
+
---
+++ javascript/datatypes.js
@@ -0,0 +1,100 @@
+//es5 > EcmaScript5
+//es6 > EcmaScript6
+////
+String > any thing in quotes
+"hiii" 'hey'  '33' "true" '33.4' `hello`
+
+Number > Any numeric value
+11 4543636 445.56 .5645 
+
+Boolean
+true false
+
+////////////////////////////////
+//es5
+var a = "Hi" 
+var b = 10
+var c = true
+
+typeof(a)
+'string'
+typeof(b)
+'number'
+typeof(c)
+'boolean'
+var d = 10.22
+typeof(d)
+'number'
+var e = "34"
+typeof(e)
+'string'
+
+var a = 10
+var b = 20
+a+b (addition)
+30
+b-a
+10
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+4%2(modules)
+0
+4%3
+1
+5%3
+2
+
+2%4
+2
+3%4
+3
+100%101
+100
+
+var a = "Hii"
+var b = "JavaScript"
+a+b(concat)
+'HiiJavaScript'
+a-b
+NaN (not a number)
+a*b
+NaN
+a/b
+NaN
+var c = 10
+undefined
+a+c
+'Hii10'
+a-c
+NaN
+
+String + String = String
+String + Number = String
+Number + String = String
+Number + Number = Number
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+'3030'
+
+"10"+20+30
+"1020"+30
+'102030'
+
+10+"20"+30-1
+102029
+
+10+20+"30"-1
+3029
+
+"10"+20+30-1
+102029

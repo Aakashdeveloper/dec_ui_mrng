2a2c43a46014978b87b950a29f7d537200971e50
aakashdeveloper <ahanda205@gmail.com>
2021-12-24 09:49:16 +0530

    alertconsole

---
+++ javascript/2datatype2.js
@@ -0,0 +1,77 @@
+var a = true
+var b = true
+a+b
+true+true
+2
+true+false
+1
+10+true
+11
+"hi"+true
+'hitrue'
+10/false
+Infinity
+
+true is equal to 1
+false is equal to 0
+
+parseInt()
+parseFloat()
+Number()
+
+var a = "10"
+var b = "2"
+
+undefined
+a+b
+'102'
+parseInt(a)+parseInt(b)
+12
+var a = "10.23"
+var b = "2.11"
+
+undefined
+parseInt(a)+parseInt(b)
+12
+parseFloat(a)+parseFloat(b)
+12.34
+var a = "hii"
+undefined
+parseInt(a)
+NaN
+
+var a = "delhi123"
+var b = "123Delhi"
+var c ="1delhi23"
+
+parseInt(a)
+NaN
+parseInt(b)
+123
+parseInt(c)
+1
+
+//es6
+var a = "10"
+undefined
+var b = "2"
+undefined
+Number(a)+Number(b)
+12
+var a = "10.11"
+undefined
+var b = "2.33"
+undefined
+Number(a)+Number(b)
+12.44
+
+var a = "delhi123"
+var b = "123Delhi"
+var c ="1delhi23"
+undefined
+Number(a)
+NaN
+Number(b)
+NaN
+Number(c)
+NaN
---
+++ javascript/3alertconsole.js
@@ -0,0 +1,34 @@
+console.log() >> deubgging
+alert > for Notification
+confirm > for input yes or no
+prompt > for taking input from user
+
+
+alert("Form Submitted")
+alert("No Slot left")
+window.alert("Tested")
+
+confirm("Do you want to leave the page")
+true
+confirm("Do you want to leave the page")
+false
+var output = confirm("Do you want to leave the page")
+undefined
+output
+true
+var output = confirm("Do you want to leave the page")
+undefined
+output
+false
+
+prompt("What is your name")
+'Aakash'
+var age = prompt("what is your age")
+undefined
+age
+'20'
+
+
+var a = prompt("Please Enter First Number")
+var b = prompt("Please Enter Second Number")
+alert(Number(a)+Number(b))

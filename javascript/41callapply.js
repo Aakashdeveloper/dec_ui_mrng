08ca65d716b63af747c5edfa337a219459febb7e
aakashdeveloper <ahanda205@gmail.com>
2022-01-20 09:36:22 +0530

    progress

---
+++ javascript/41callapply.js
@@ -0,0 +1,40 @@
+var person = {
+    fullname: function(){
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
+
+var John = {
+    firstname:'John',
+    lastname:'Dave'
+}
+
+person.fullname.call(John)
+'John Dave'
+person.fullname.apply(John)
+'John Dave'
+
+
+
+var person = {
+    fullname: function(city,country){
+        return `${this.firstname} ${this.lastname} ${city} ${country}`;
+    }
+}
+
+var John = {
+    firstname:'John',
+    lastname:'Dave'
+}
+
+person.fullname.call(John,'Delhi','India')
+'John Dave Delhi India'
+person.fullname.apply(John,'Delhi','India')
+VM49:1 Uncaught TypeError: CreateListFromArrayLike called on non-object
+    at <anonymous>:1:17
+(anonymous) @ VM49:1
+
+person.fullname.call(John,['Delhi','India'])
+'John Dave Delhi,India undefined'
+person.fullname.apply(John,['Delhi','India'])
+'John Dave Delhi India'
